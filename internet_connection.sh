e915e020fd6096b29b1d8377a5258abdaca2bb8a
he11ok <43732454+he11ok@users.noreply.github.com>
2020-04-03 14:14:45 +0100

    keep ping the website once connected, excute
    add  
    bash /root/internet_connection.sh
     in 
    /etc/rc.local  
    before
    exit 0

---
+++ internet_connection.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /home/pi/pycode/
+while :
+do
+	ping -c 2 www.google.com  &>/dev/null
+	[ $? -eq 0 ] && break
+done	
+ 
+a='ifconfig wlan0 | grep broadcast'
+python3 /home/pi/pycode/email_IP_qq.py "wlan0:$a"
+echo "ok"
