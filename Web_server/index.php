1723913c55be97819cc284288beac3c19ec83347
he11ok <43732454+he11ok@users.noreply.github.com>
2020-08-17 04:48:24 +0100

    Add files via upload

---
+++ Web_server/index.php
@@ -0,0 +1,48 @@
+
+<?php 
+	/*  phpinfo(); */
+	echo "<h2>RPI swarm robot</h2><br>";
+	echo "Hello world, this is a test \n for RPI web sever via php";
+	echo "<br>";echo "<br>";echo "<br>";
+
+	$domain = "0bit.top";
+
+	echo "picture 1: our RPI robot (video demo possible)";
+	echo "<br>";echo "<br>";
+	echo "Link 1: to pdf -- user manual";
+	echo "<br>";echo "<br>";
+	echo "Link 2: lab website";
+	echo "<br>";echo "<br>";
+	echo "Link 3: work status of the swarm -- link to MySQL database";
+	echo "<br>";echo "<br>";
+
+	echo "control panel:";
+	echo "<br>";echo "<br>";
+	echo "button 1: random work";
+	echo "<br>";echo "<br>";
+	echo "button 2: change color";
+	echo "<br>";echo "<br>";
+	echo "button 3: report status";
+	echo "<br>";echo "<br>";
+	echo "button 4: stop working";
+
+	echo"<h2>PHP tutorial learning</h2><br>";
+	$x = 5;
+	$y = 10;
+	print "$x + $y = ".($x + $y)."<br>";
+	var_dump($x);
+	echo "<br>";
+	var_dump($domain);
+	echo "<br>";
+	echo strlen($domain);
+	$hi = "hello world, hello, hi!";
+	echo "<br>";
+	echo str_word_count($hi);
+	echo "<br>";
+	echo "Today is " . date("Y/m/d") . "<br>";
+	echo "Today is " . date("Y.m.d") . "<br>";
+	echo "Today is " . date("Y-m-d") . "<br>";
+	echo "Today is " . date("l");
+	
+
+?>
